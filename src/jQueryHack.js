fbe89c5ebffc20897ff79932a5fe8f8b1ad1b43e
Tom Wey <tjmwey@gmail.com>
2017-11-24 16:50:14 +0000

    Subscribe to live bus updates
    This is gross since hubs/signalr is all about using a single global
    object to handle things.
    
    We’re not merging the updates as we receive them with existing data,
    which we need to.

---
+++ src/jQueryHack.js
@@ -0,0 +1,5 @@
+import $ from "jquery";
+
+if (!window.jQuery) {
+  window.jQuery = $;
+}
--- src/redux/modules/predictions/actions.js
+++ src/redux/modules/predictions/actions.js
@@ -1,11 +1,43 @@
+import "../../../jQueryHack";
+import "signalr";
+import "../../../tflHubs";
+
 export const PREDICTIONS_REQUESTED = 'counter/PREDICTIONS_REQUESTED'
 export const UPDATE_PREDICTIONS = 'counter/UPDATE_PREDICTIONS'
+
+const $ = window.jQuery;
+
+$.connection.hub.url = "https://push-api.tfl.gov.uk/signalr/hubs/signalr";
+
+const hub = $.connection.predictionsRoomHub;
+
+const toLowerFirst = (string) => {
+  return string.charAt(0).toLowerCase() + string.slice(1);
+}
 
 export const requestStopPredictions = (naptanId) => {
   return (dispatch) => {
     const appId = process.env.REACT_APP_TFL_APP_ID;
     const appKey = process.env.REACT_APP_TFL_APP_KEY;
     const url = `https://api.tfl.gov.uk/StopPoint/${naptanId}/Arrivals?mode=bus&app_id=${appId}&app_key=${appKey}`;
+
+    // Push notification callback
+    hub.client.showPredictions = predictions => {
+      console.log("🚌 New predictions @", new Date().toTimeString());
+      console.table(predictions, ["LineName", "VehicleId", "DestinationName", "ExpectedArrival", "TimeToLive", "Id"]);
+
+      const transformedPredictions = predictions.map((p) => {
+        return Object.keys(p).reduce((acc, key) => {
+          acc[toLowerFirst(key)] = p[key];
+          return acc;
+        }, {})
+      });
+
+      dispatch({
+        type: UPDATE_PREDICTIONS,
+        predictions: transformedPredictions,
+      });
+    }
 
     dispatch({
       type: PREDICTIONS_REQUESTED
@@ -15,6 +47,12 @@ export const requestStopPredictions = (naptanId) => {
       .then(function(response) {
         return response.json();
       }).then(function(json) {
+        $.connection.hub.start().done(() => {
+          const lineRooms = [{ "NaptanId": naptanId }];
+          console.log("Registering for updates: " + naptanId);
+          hub.server.addLineRooms(lineRooms)
+        });
+
         return dispatch({
           type: UPDATE_PREDICTIONS,
           predictions: json,
